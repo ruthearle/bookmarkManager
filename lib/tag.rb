61ed9da13f555fe8306ec53a959c3ca598000e87
Ruth Earle <CarpeDiem@Ruths-MacBook-Air.local>
2014-11-19 12:25:04 +0000

    refactored add_link_spec. tests still passing

--- lib/tag.rb
+++ lib/tag.rb
@@ -2,11 +2,10 @@ class Tag
 
   include DataMapper::Resource
 
+  has n, :links, :through => Resource
 
   property :id, Serial
   property :text, String
-
-  has n, :links, :through => Resource
 
 end
 
