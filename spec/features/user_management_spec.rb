73376718c99820cfb856fb6b42323c24bfe9ccbc
Ruth Earle <rrearle@gmail.com>
2014-11-19 14:02:02 +0000

    Refactored tests. Extracted helper methods into user_helper.rb. Tests passing.

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'helper/user_helper'
 
 feature "User signs up" do
 
@@ -20,15 +21,6 @@ feature "User signs up" do
     expect(page).to have_content("This email is already taken")
   end
 
-  def sign_up(email                 = "alice@example.com",
-              password              = "oranges!",
-              password_confirmation = "oranges!")
-    visit "/users/new"
-    fill_in :email, :with => email
-    fill_in :password, :with => password
-    fill_in :password_confirmation, :with => password_confirmation
-    click_button "Sign up"
-  end
 end
 
 feature "User signs in" do
@@ -51,13 +43,6 @@ feature "User signs in" do
     expect(page).not_to have_content("Welcome, test@test.com")
     sign_in("test@test.com", "wrong")
     expect(page).not_to have_content("Welcome, test@test.com")
-  end
-
-  def sign_in(email, password)
-    visit "/sessions/new"
-    fill_in "email", :with => email
-    fill_in "password", :with => password
-    click_button "Sign in"
   end
 
 end
